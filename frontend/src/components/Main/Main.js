1d3ed4e87881581722206fbb04cebe045c7fcb96
Prokud1n <vaprokudin@edu.hse.ur>
2023-11-11 22:10:35 +0400

    init useWebSocket

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,15 +1,34 @@
 import DataPopup from "../DataPopup/DataPopup";
-import Map from "../Map/Map";
 import "./Main.css";
+import React, {useCallback, useState} from "react";
+import Map from "../Map/Map";
+import useWebSocket from "../../hooks/useWebSocket";
 
 function Main() {
+    const [data, setData] = useState([]);
+    const handleWebSocketData = useCallback((data) => {
+        setData((prevTrains) => {
+            const trainIndex = prevTrains.findIndex((train) => train.train_index === data.train_index);
+
+            if (trainIndex !== -1) {
+                prevTrains[trainIndex] = data;
+                return [...prevTrains];
+            } else {
+                return [...prevTrains, data];
+            }
+        });
+    }, []);
+
+    useWebSocket(handleWebSocketData);
+
+    console.log("MAIN", data);
   return (
     <main className="main">
-      <Map />
+      <Map data={data} />
       <DataPopup position="left" />
       <DataPopup position="right" />
     </main>
   );
 }
 
-export default Main;
+export default React.memo(Main);
